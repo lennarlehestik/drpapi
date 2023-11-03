03c4fd36ca70213f344c9cc46023710a92460b45
lennarlehestik <lehestiklennar@gmail.com>
2023-11-03 11:48:24 +0200

    ''

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors({
   origin: ['http://localhost:3000',
-          'https://disputeresolutionproject.web.app/']
+          'https://disputeresolutionproject.web.app']
 }));
 
 const storage = multer.memoryStorage(); // Store the file in memory
